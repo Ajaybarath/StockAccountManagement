1d84b89597fd5dd1f507b3e32f2d71cf4a61413e
ajaybarath <ajaybarath3@gmail.com>
2021-09-05 18:19:10 +0530

    ADDED: list to maintain the stock transaction

--- src/com/bridgelabz/stockManagement/Stock.java
+++ src/com/bridgelabz/stockManagement/Stock.java
@@ -1,16 +1,21 @@
 package com.bridgelabz.stockManagement;
 
+import java.util.ArrayList;
+
 public class Stock {
 
 	private String name;
 	private int noOfShares;
 	private int price;
 
+	ArrayList<String> transaction;
+
 	public Stock(String name, int noOfShares, int price) {
 		super();
 		this.name = name;
 		this.noOfShares = noOfShares;
 		this.price = price;
+		transaction = new ArrayList<>();
 	}
 
 	public void setName(String name) {
@@ -41,7 +46,13 @@ public class Stock {
 	public String toString() {
 		return "Stock [name=" + name + ", noOfShares=" + noOfShares + ", price=" + price + "]";
 	}
-	
-	
+
+	public ArrayList<String> getTransaction() {
+		return transaction;
+	}
+
+	public void setTransaction(String data) {
+		transaction.add(data);
+	}
 
 }
