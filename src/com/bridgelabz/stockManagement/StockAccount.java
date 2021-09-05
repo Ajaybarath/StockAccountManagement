f79edc225386e58508a20fc51cededcf30694d37
ajaybarath <ajaybarath3@gmail.com>
2021-09-05 18:14:21 +0530

    ADDED: function to sell shares

--- src/com/bridgelabz/stockManagement/StockAccount.java
+++ src/com/bridgelabz/stockManagement/StockAccount.java
@@ -22,12 +22,34 @@ public class StockAccount {
 
 	
 	void buy(int amount, String symbol) {
-		
-		
+		if (stockList.containsKey(symbol)) {
+			Stock stock = stockList.get(symbol);
+			int shareCount = stock.getNoOfShares();
+			shareCount += amount;
+			stock.setNoOfShares(shareCount);
+			stockList.put(symbol, stock);
+		}
+
+		System.out.println(amount + symbol + " shares bought ");
 	}
 	
 	void sell(int amount, String symbol) {
-		
+		if (stockList.containsKey(symbol)) {
+			Stock stock = stockList.get(symbol);
+			int shareCount = stock.getNoOfShares();
+			if (shareCount >= amount) {
+				shareCount -= amount;
+				stock.setNoOfShares(shareCount);
+				stockList.put(symbol, stock);
+			}
+			else {
+				System.out.println("Not enough shares availaible to sell");
+				return;
+			}
+			
+		}
+
+		System.out.println(amount + symbol + " shares sold ");
 	}
 	
 	void printReport() {
